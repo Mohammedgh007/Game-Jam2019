1e8fc305c60626dd5563bc3970cff57053b11fd3
Mohammedgh007 <mohammedgh007@gmail.com>
2019-10-04 22:32:16 -0700

    finishing character movement and starting combat of the hero.

---
+++ The Wizard and the Curse of Nothingness/Assets/Scripts/CharacterCombat.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+Author: Mohammed Alghamdi
+Popuse: this file encapsulates all the code related to the hero fighting and 
+  defending by a shield.
+ */
+public class CharacterCombat : MonoBehaviour
+{
+
+    public bool isShielded;
+    public bool isShieldedInitial; // retrieved from the memory
+    public bool isAttacking; // close range attacking
+    public bool isAttackingInitial; // retrieved from the memory
+    public bool isShooting; // far range attacking
+    public bool isShootingInitial; // retrieved from the memory
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isShieldedInitial = true;
+        isShielded = false;
+        isAttackingInitial = true;
+        isAttacking = false;
+        isShootingInitial = true;
+        isShooting = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+            attack();
+        if(Input.GetKey(KeyCode.Mouse1))
+            shoot();
+        if(Input.GetKey(KeyCode.Space))
+            shield();
+    }
+
+
+    // this method establishes the event of attack(close range)
+    private void attack() {
+        //TODO animation
+    }
+
+    // this method establishes the event of sheild
+    private void shield() {
+        //TODO animation
+    }
+
+    // this method establishes the event of shooting(far range attack)
+    private void shoot() {
+        
+    }
+}
---
+++ The Wizard and the Curse of Nothingness/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+Author: Mohammed Alghamdi
+Popuse: this file encapsulates all the code related to the hero movement.
+ */
+public class PlayerMovement : MonoBehaviour
+{
+    // horizental and veritcal coordinates at the beginning of the level
+    private float xCoordOrigin;
+    private float yCoordOrigin;
+
+    // horizental and veritcal coordinates currently
+    private float xCoordCurr;
+    private float yCoordCurr;
+
+    // those 2 variables assures that the hero movements will be smooth.
+    public float lerbRate, speed;
+    private bool canDash; // retrieved from the memory
+
+
+    // those variables are used for controlling the jump
+    private bool isGrounded;
+    private bool canDoubleJump;
+    private bool canDoubleJumpInitial; // retrieved from the memory
+    public float initialJumpRate, doubleJumpRate;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        xCoordCurr = transform.position.x;
+        xCoordOrigin = transform.position.x;
+        yCoordCurr = transform.position.y;
+        yCoordOrigin = transform.position.y;
+        isGrounded = true;
+        canDoubleJumpInitial = true;
+        canDoubleJump = canDoubleJumpInitial;
+        canDash = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // updating coordinates
+        yCoordCurr = transform.position.y;
+        xCoordCurr = transform.position.x;
+
+        // horizontal movements
+        if (Input.GetKey(KeyCode.D))
+            stepRight(); 
+        else if (Input.GetKey(KeyCode.A)) 
+            stepLeft();
+        // dashing
+        if (Input.GetKeyDown("s") && canDash)
+            speed = speed * 3f;
+        else if (Input.GetKeyUp("s") && canDash)
+            speed = speed / 3f;
+        
+        // vartical movements
+        if (Input.GetKeyDown("w"))
+            jump();
+    }
+
+
+    // make the character move one step to the right.
+    private void stepRight() {
+        // TODO rotate right
+        xCoordCurr += speed;
+        Vector3 tempPos = new Vector3(transform.position.x, yCoordCurr, 0);
+        Vector3 targetPos = new Vector3(xCoordCurr, yCoordCurr, 0);
+        tempPos = Vector3.Lerp(tempPos, targetPos, lerbRate * Time.deltaTime);;
+        transform.position = tempPos;
+    }
+
+
+    // make the character move one step to the left.
+    private void stepLeft() {
+        // TODO rotate left
+        xCoordCurr -= speed;
+        Vector3 tempPos = new Vector3(transform.position.x, yCoordCurr, 0);
+        Vector3 targetPos = new Vector3(xCoordCurr, yCoordCurr, 0);
+        tempPos = Vector3.Lerp(tempPos, targetPos, lerbRate * Time.deltaTime);;
+        transform.position = tempPos;
+    }
+
+
+    // let the character do the jump whether it is a single or a double.
+    private void jump() {
+        if (isGrounded) {
+          rb.AddForce (0f, initialJumpRate, 0f); 
+          isGrounded = false;
+        } else if (canDoubleJump) {
+            rb.AddForce (0f, doubleJumpRate, 0f);
+            canDoubleJump = false;
+        }
+    }
+
+
+    
+    void OnCollisionEnter (Collision other){
+		isGrounded = true;
+        canDoubleJump = canDoubleJumpInitial;
+	}
+ 
+}
